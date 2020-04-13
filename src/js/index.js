0f3a781f3c927c99d52f384f1ad23be82f67127a
khvalkoilya <khvalko.ilya@gmail.com>
2020-04-13 14:50:51 +0300

    feat: add side menu withou links, add animation for burger

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,25 @@ import cards from './cards.js';
 
 const checkbox = document.querySelector('.checkbox__input');
 const checkboxText = document.querySelector('.checkbox__text');
+let isTrain = true;
 checkbox.addEventListener('change',()=>{
+    isTrain=!isTrain;
     if(checkbox.checked) {
         checkboxText.classList.remove('train');
         checkboxText.classList.add('play');
+        document.querySelector('.side-menu').classList.add('side-menu_play');
     }
     else {
         checkboxText.classList.remove('play');
         checkboxText.classList.add('train');
+        document.querySelector('.side-menu').classList.remove('side-menu_play');
     }
 })
+
+document.querySelector('.burger').addEventListener('click', sideMenu);
+
+function sideMenu() {
+    document.querySelector('.side-menu').classList.toggle('side-menu_active');
+    document.querySelector('.burger').classList.toggle('burger_active')
+}
 
