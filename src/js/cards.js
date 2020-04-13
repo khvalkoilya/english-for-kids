2ff5865f018aed31b6106a0e6ea9cab848734c9a
khvalkoilya <khvalko.ilya@gmail.com>
2020-04-13 21:11:13 +0300

    feat: add closing of side menu, make visual of main page

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,5 +1,38 @@
 const cards = [
-  ['Animals', 'Rooms', 'Actions', 'Vegetables', 'Fruits', 'Colors', 'Decor', 'Food'],
+  [ 
+    {
+        name: 'Animals',
+        image: 'assets/images/animals.jpg'
+    },
+    {
+        name: 'Rooms',
+        image: 'assets/images/rooms.jpg'
+    },
+    {
+        name: 'Actions',
+        image: 'assets/images/actions.jpg'
+    },
+    {
+        name: 'Vegetables',
+        image: 'assets/images/vegetables.jpg'
+    },
+    {
+        name: 'Fruits',
+        image: 'assets/images/fruits.jpg'
+    },
+    {
+        name: 'Colors',
+        image: 'assets/images/colors.jpg'
+    },
+    {
+        name: 'Decor',
+        image: 'assets/images/decor.jpg'
+    },
+    {
+        name: 'Food',
+        image: 'assets/images/food.jpg'
+    }
+  ],
   [
     {
         
--- src/js/index.js
+++ src/js/index.js
@@ -20,14 +20,23 @@ checkbox.addEventListener('change',()=>{
         checkboxText.classList.add('train');
         document.querySelector('.side-menu').classList.remove('side-menu_play');
     }
+    if(currentPage === "Main Page") document.querySelectorAll('.topic-card').forEach((item)=>item.classList.toggle('topic-card_play'));
 });
 
-
 document.querySelector('.burger').addEventListener('click', sideMenu)
- function sideMenu() {
+function sideMenu() {
     document.querySelector('.side-menu').classList.toggle('side-menu_active');
-    document.querySelector('.burger').classList.toggle('burger_active')
+    document.querySelector('.burger').classList.toggle('burger_active');
 };
+
+
+document.querySelector('body').addEventListener('click', (e)=>{
+    if(document.querySelector('.side-menu').classList.contains('side-menu_active') && (!e.target.classList.contains('burger') && !e.target.parentElement.classList.contains('burger'))) {
+        document.querySelector('.side-menu').classList.remove('side-menu_active');
+        document.querySelector('.burger').classList.remove('burger_active');
+    }
+
+})
 
 document.querySelectorAll(".link").forEach((elem)=>elem.addEventListener('click', ()=>{
     document.querySelectorAll(".link").forEach((e)=>e.classList.remove('link_active'));
@@ -41,10 +50,11 @@ document.querySelectorAll(".link").forEach((elem)=>elem.addEventListener('click'
 function mainPageBlock() {
     const page = document.createElement('section');
     page.className="main-page";
-    cards[0].forEach((name) => {
+    cards[0].forEach((item) => {
         const card = document.createElement('div');
         card.className = 'topic-card';
-        card.innerHTML = `<p class="topic-card__name">${name}</p>`;
+        card.innerHTML = `<div class="topic-card__image-block"><img class="topic-card__image-block__image" src="${item.image}"></img></div>`
+        card.innerHTML += `<p class="topic-card__name">${item.name}</p>`;
         page.append(card);
     });
     console.log(page)
