3bf77e353dc09c143722345cf725a96b464b60df
khvalkoilya <khvalko.ilya@gmail.com>
2020-04-13 18:56:00 +0300

    feat: add links to side menu, begin main page in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,10 @@ import cards from './cards.js';
 const checkbox = document.querySelector('.checkbox__input');
 const checkboxText = document.querySelector('.checkbox__text');
 let isTrain = true;
+let currentPage="Main Page";
+
+mainPageBlock();
+
 checkbox.addEventListener('change',()=>{
     isTrain=!isTrain;
     if(checkbox.checked) {
@@ -16,12 +20,33 @@ checkbox.addEventListener('change',()=>{
         checkboxText.classList.add('train');
         document.querySelector('.side-menu').classList.remove('side-menu_play');
     }
-})
+});
 
-document.querySelector('.burger').addEventListener('click', sideMenu);
 
-function sideMenu() {
+document.querySelector('.burger').addEventListener('click', sideMenu)
+ function sideMenu() {
     document.querySelector('.side-menu').classList.toggle('side-menu_active');
     document.querySelector('.burger').classList.toggle('burger_active')
-}
+};
 
+document.querySelectorAll(".link").forEach((elem)=>elem.addEventListener('click', ()=>{
+    document.querySelectorAll(".link").forEach((e)=>e.classList.remove('link_active'));
+    elem.classList.add('link_active');
+    sideMenu();
+    currentPage = elem.innerHTML;
+    if(currentPage == "Main Page") mainPageBlock();
+    else cardPageBlock();
+}));
+
+function mainPageBlock() {
+    const page = document.createElement('section');
+    page.className="main-page";
+    cards[0].forEach((name) => {
+        const card = document.createElement('div');
+        card.className = 'topic-card';
+        card.innerHTML = `<p class="topic-card__name">${name}</p>`;
+        page.append(card);
+    });
+    console.log(page)
+    document.querySelector('main > .wrapper').append(page);
+};
