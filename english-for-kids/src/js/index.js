abd574b6c09aec1d3c209f8318b13f5322a0db62
IlliaKhvalko <khvalko.ilya@gmail.com>
2020-04-17 18:00:19 +0300

    feat: add rooms, actions. add place for rating and add some styles for play mode

--- english-for-kids/src/js/index.js
+++ english-for-kids/src/js/index.js
@@ -19,8 +19,13 @@ checkbox.addEventListener('change', () => {
     checkboxText.classList.add('train');
     document.querySelector('.side-menu').classList.remove('side-menu_play');
   }
-  if (currentPage === 'Main Page') document.querySelectorAll('.topic-card').forEach((item) => item.classList.toggle('topic-card_play'));
+  if (currentPage === 'Main Page') changeModeForMain();
+  else changeModeForTopic();
 });
+
+function changeModeForMain() {
+  document.querySelectorAll('.topic-card').forEach((item) => item.classList.toggle('topic-card_play'));
+}
 
 document.querySelector('.burger').addEventListener('click', sideMenu);
 function sideMenu() {
@@ -44,7 +49,7 @@ function changeActiveLink(elem) {
   sideMenu();
   currentPage = elem.innerHTML;
   if (currentPage == 'Main Page') mainPageBlock();
-  else findPositionOfTopicInLinks(currentPage);
+  else findPositionOfTopic(currentPage);
 }
 
 function mainPageBlock() {
@@ -58,18 +63,26 @@ function mainPageBlock() {
     cards.push(topicCard);
   })
   let section = create('section', 'main-page',cards,document.querySelector('main'));
-  if (!isTrain) document.querySelectorAll('.topic-card').forEach((item) => item.classList.toggle('topic-card_play'));
-  document.querySelectorAll('.topic-card').forEach((item) => item.addEventListener('click', () => findPositionOfTopicInMainPage(item)));
+  if (!isTrain) changeModeForMain();
+  topicCard();
 }
 
 
-function findPositionOfTopicInMainPage(item) {
-  const positionOfTopic = list.topics.indexOf(list.topics.find((obj) => obj.name === item.children[1].innerHTML));
-  topicPageBlock(positionOfTopic);
+function topicCard() {
+  document.querySelectorAll('.topic-card').forEach((item) => item.addEventListener('click', () => {
+    findPositionOfTopic(item.children[1].innerHTML);
+    document.querySelectorAll('.link').forEach((e) => {
+      if(e.innerHTML!==item.children[1].innerHTML)
+        e.classList.remove('link_active');
+      else e.classList.add('link_active');
+      });
+  }));
 }
 
-function findPositionOfTopicInLinks(item) {
+
+function findPositionOfTopic(item) {
   const positionOfTopic = list.topics.indexOf(list.topics.find((obj) => obj.name === item));
+  currentPage = item;
   topicPageBlock(positionOfTopic);
 }
 
@@ -77,9 +90,20 @@ function topicPageBlock(position) {
   document.querySelector('main').innerHTML = '';
   const objOfCards = new WordCards(list.cards[position]);
   const arrayOfCards = objOfCards.createWordCard();
-
-  const page = create('section', 'topic-page', create('div','topic-page__table',arrayOfCards));
+  const page = create('section', 'topic-page', [create('div', 'rating none'), create('div','topic-page__table',arrayOfCards), create('div', 'buttons none'), create('audio', 'sound'), create('audio', 'effect')]);
   document.querySelector('main').append(page);
-
   document.querySelectorAll('.word-card__refresh').forEach((item)=>item.addEventListener('click', () => item.parentElement.parentElement.classList.add('rotate')));
+  if(!isTrain) changeModeForTopic();
+}
+
+function changeModeForTopic() {
+  replacer('.word-card__image-block', 'word-card__image-block_play');
+  replacer('.word-card__image-block__image', 'word-card__image-block__image_play');
+  replacer('.word-card__name', 'none');
+  replacer('.word-card__refresh', 'none');
+  replacer('.rating', 'none');
+  replacer('.buttons', 'none')
+  function replacer(what, how) {
+    document.querySelectorAll(what).forEach((item) => item.classList.toggle(how));
+  }
 }
--- english-for-kids/src/js/layouts/cards.js
+++ english-for-kids/src/js/layouts/cards.js
@@ -51,60 +51,102 @@ export default [
     ],
     [
       {
-        name: 'Kitchen',
-        // image: ,
-        // translation: ,
+        name: 'kitchen',
+        image: '../src/assets/images/kitchen.jpg',
+        translation: 'кухня',
         // audio:
       },
       {
-  
+        name: 'bathroom',
+        image: '../src/assets/images/bathroom.jpg',
+        translation: 'ванная',
+        // audio:
       },
       {
-  
+        name: 'bedroom',
+        image: '../src/assets/images/bedroom.jpg',
+        translation: 'спальная комната',
+        // audio:
       },
       {
-  
+        name: 'dining room',
+        image: '../src/assets/images/diningroom.jpg',
+        translation: 'столовая',
+        // audio:
       },
       {
-  
+        name: 'living room',
+        image: '../src/assets/images/livingroom.jpg',
+        translation: 'гостинная',
+        // audio:
       },
       {
-  
+        name: 'attic',
+        image: '../src/assets/images/attic.jpg',
+        translation: 'чердак',
+        // audio:
       },
       {
-  
+        name: 'utility room',
+        image: '../src/assets/images/utilityroom.jpg',
+        translation: 'подсобное помещение',
+        // audio:
       },
       {
-  
+        name: 'wardrobe',
+        image: '../src/assets/images/wardrobe.jpg',
+        translation: 'гардеробная',
+        // audio:
       },
     ],
     [
         {
-          name: 'Kitchen',
-          // image: ,
-          // translation: ,
-          // audio:
+            name: 'cry',
+            image: '../src/assets/images/cry.jpg',
+            translation: 'плакать',
+            // audio:
         },
         {
-    
+            name: 'dance',
+            image: '../src/assets/images/dance.jpg',
+            translation: 'танцевать',
+            // audio:
         },
         {
-    
+            name: 'sing',
+            image: '../src/assets/images/sing.jpg',
+            translation: 'петь',
+            // audio:
         },
         {
-    
+            name: 'swim',
+            image: '../src/assets/images/swim.jpg',
+            translation: 'плавать',
+            // audio:
         },
         {
-    
+            name: 'run',
+            image: '../src/assets/images/run.jpg',
+            translation: 'бегать',
+            // audio:
         },
         {
-    
+            name: 'play',
+            image: '../src/assets/images/play.jpg',
+            translation: 'играть',
+            // audio:
         },
         {
-    
+            name: 'jump',
+            image: '../src/assets/images/jump.jpg',
+            translation: 'прыгать',
+            // audio:
         },
         {
-    
+            name: 'drive',
+            image: '../src/assets/images/drive.jpg',
+            translation: 'водить машину',
+            // audio:
         },
       ],
       [
